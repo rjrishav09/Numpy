a212500fdbe1657cada4558162d672eb987ab1d5
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-20 20:38:01 +0530

    First commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# N-Dimensional array:
 import numpy as np
 array = np.array([1,2,3])
 array = array*2
 print(array)
+
+# Multi-Dimensional array:
+array1 = np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
+                    [["J","K","L"],["M","N","O"],["P","Q","R"]],
+                    [["S","T","U"],["V","W","X"],["Y","Z","ZA"]]])
+# Chain Indexing
+print(array1[0][0][0])
