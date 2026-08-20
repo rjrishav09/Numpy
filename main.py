6ddea61f43fa72c31065e0f79c53e19673d9ed79
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-20 20:56:42 +0530

    Adding files

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# N-Dimensional array:
+# 1. N-Dimensional array:
 import numpy as np
 array = np.array([1,2,3])
 print(array)
 array = array*2
 print(array)
 
-# Multi-Dimensional array:
+# 2. Multi-Dimensional array:
 array1 = np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
                     [["J","K","L"],["M","N","O"],["P","Q","R"]],
                     [["S","T","U"],["V","W","X"],["Y","Z","ZA"]]])
@@ -27,3 +27,15 @@ print(array1.ndim)
 # Check the shape of array:
 print(array.shape)
 print(array1.shape)
+
+# 3. Slicing: array(start:end:stop)
+array2 = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9],])
+print(array2.shape)
+
+# Printing whole array:
+print(array2[0:3])
+
+# Printing array with 2 steps:
+print(f" Your array is : {array2[::2]}")
