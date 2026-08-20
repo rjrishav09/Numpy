5aa73a324fa2a7c485bc74300a2448fe41e7fc69
Rishav Bhardwaj <crazyrishavhacker@gmail.com>
2026-08-20 20:46:25 +0530

    adding files

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ array1 = np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
                     [["S","T","U"],["V","W","X"],["Y","Z","ZA"]]])
 # Chain Indexing
 print(array1[0][0][0])
+
+# Multi-Dimensional Indexing
+
+print(array1[0,0,0])
+
+# Word Formation using Catenation:
+word = array1[0,0,0] + array1[1,2,2] + array1[2,2,1] + array1[2,0,2] + array1[1,1,1]
+print(f"Your name is: {word}")
+
+# Check the number of dimension:
+print(array.ndim)
+print(array1.ndim)
+
+# Check the shape of array:
